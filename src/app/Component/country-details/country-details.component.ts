52843d7e4f242a42e579d17b0c3abb261fe0b479
Vishnutharan <karunakaran.vishnu6@gmail.com>
2024-12-21 17:24:36 +0530

    AdditionalChanges

--- src/app/Component/country-details/country-details.component.ts
+++ src/app/Component/country-details/country-details.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TravelService } from 'src/app/Service/travel.service';
-import { Country } from 'src/app/Interface/travel.interface';
+import { CartItem, Country, TouristPlace } from 'src/app/Interface/travel.interface';
 
 @Component({
   selector: 'app-country-details',
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.css']
+  styleUrls: ['./country-details.component.css'],
 })
 export class CountryDetailsComponent implements OnInit {
   countryId!: string;
   countryDetails!: Country;
+  touristPlaces: TouristPlace[] = [];
+  cartItems: CartItem[] = [];
+  totalAmount = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -18,11 +21,56 @@ export class CountryDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Retrieve country ID from the route
     this.countryId = this.route.snapshot.paramMap.get('id')!;
-    // Fetch country details from the service
+    this.loadCountryDetails();
+    this.loadTouristPlaces();
+    this.loadCartItems();
+  }
+
+  private loadCountryDetails(): void {
     this.travelService.getCountryById(this.countryId).subscribe(details => {
       this.countryDetails = details;
     });
+  }
+
+  private loadTouristPlaces(): void {
+    this.travelService.getTouristPlaces(this.countryId).subscribe(places => {
+      this.touristPlaces = places;
+    });
+  }
+
+  private loadCartItems(): void {
+    this.travelService.getCartItems().subscribe(items => {
+      this.cartItems = items;
+      this.calculateTotal();
+    });
+  }
+
+  addToCart(place: TouristPlace): void {
+    this.travelService.addToCart(place);
+  }
+
+  removeFromCart(placeId: string): void {
+    this.travelService.removeFromCart(placeId);
+  }
+
+  updateQuantity(placeId: string, quantity: number): void {
+    if (quantity > 0) {
+      this.travelService.updateQuantity(placeId, quantity);
+    }
+  }
+
+  calculateTotal(): void {
+    this.totalAmount = this.cartItems.reduce((total, item) => 
+      total + (item.cost * item.quantity), 0);
+  }
+
+  printItinerary(): void {
+    window.print();
+  }
+
+  proceedToPayment(): void {
+    // Implement payment gateway integration
+    console.log('Proceeding to payment');
   }
 }
